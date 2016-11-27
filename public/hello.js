6f7f120eed21593cd7d6c0f9552874fbea2615ce
jcotzin <jcotzin@gmail.com>
2016-11-27 13:50:59 -0500

    added new project

--- public/hello.js
+++ public/hello.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+    $(".pclass").fadeIn(2000);
     $('.hover').bind('touchstart touchend', function(e) {
         // e.preventDefault();
         $(this).toggleClass('hover_effect');
     });
+
+
+
 });
